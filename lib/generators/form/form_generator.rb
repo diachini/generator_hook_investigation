e3830941e3a850596324403ceb4eef0790eaf296
diachini <danny@jetpackworkflow.com>
2021-03-24 11:56:02 -0400

    Make attributes available to the generator
    In order to access the attributes specified via the command-line, this
    introduces the `argument` and associated `attribute_names` method the
    same way that active_model_serializers does it:
    
    https://github.com/rails-api/active_model_serializers/blob/0-10-stable/lib/generators/rails/serializer_generator.rb

--- lib/generators/form/form_generator.rb
+++ lib/generators/form/form_generator.rb
@@ -1,9 +1,20 @@
 class FormGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('templates', __dir__)
+  check_class_collision suffix: 'Form'
+
+  argument :attributes, type: :array, default: [], banner: 'field:type field:type'
+
+  class_option :parent, type: :string, desc: 'The parent class for the generated form'
 
   def create_form_file
     template 'form.erb', File.join('app/forms', class_path, "#{file_name}_form.rb")
   end
 
   hook_for :test_framework
+
+  private
+
+  def attributes_names
+    [:id] + attributes.reject(&:reference?).map! { |a| a.name.to_sym }
+  end
 end
